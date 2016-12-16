6453e281d659135d656a04d7e8c8a8a713f0bdd7
Matthew Biesen <upandcomming88@gmail.com>
2016-12-16 07:52:14 +0000

    Update ledfromemail.py

--- ledfromemail.py
+++ ledfromemail.py
@@ -1,20 +1,27 @@
 #This python script is intended to convert the javascript output, which is in rgb format and with undesirable xy coordinates
 #information on matrix coordinates can be obtained from https://learn.adafruit.com/32x16-32x32-rgb-led-matrix/library
 
+#as noted below in imports, requires adafruit RGBMatrix library to operate
+#NOTE: 
+#NOTE: adafruit 'rgbmatrix.so' should be housed in the same folder as your scripts.
+#NOTE: the mailtestimaplib refers to the 'mailtestimaplib.py' file located in this repository
+#The mailtestimpalib.py file NEEDS to be located in the same folder as this file.
+
 import time
+import os
 from rgbmatrix import Adafruit_RGBmatrix
 import mailtestimaplib
-import os
-#NOTE: adafruit library should be housed in the same folder as your scripts.
 
-matrix = Adafruit_RGBmatrix(32, 1)
+
+
+matrix = Adafruit_RGBmatrix(32, 1)#declares one matrix that is 32 by 32
 
 def readinfofromcycledfile(filetoopen):
   #open the file
   fromemailfile = open(filetoopen)#for windows users, format is C:\\folder\\...\\file.extension
   fromemailread = fromemailfile.read()
   fromemailarray = fromemailread.split(';')#we should now hoave an array, where each array item consists of xy and color
-  return fromemailarray
+  return fromemailarray #our array now has all colons and semicolons removed
 
 def formatmydata(revisedarray):
   alpharray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F']
@@ -42,7 +49,7 @@ def formatmydata(revisedarray):
       else:
         tempy = tempy + thischar
 
-    tempx = int(tempx)-1#because arrays are zero indexed, the js was not    
+    tempx = int(tempx)-1#because arrays are zero indexed, the js was not, reduce tempx by 1    
     xarray.append(tempx)
     yarray.append(tempy)
 
@@ -85,6 +92,7 @@ def formatmydata(revisedarray):
   print(newcolorarray)
   #now colors need to be reduced for the 7,7,7 format, while being kept in sets of three
   #UPDATE: This is not true, colors do not need to be reduced
+  #NOTE: this was retained for potential arduino use per online direction.
   ##n = 0
   ##ctr = 0
   ##while n < len(newcolorarray):
@@ -95,6 +103,7 @@ def formatmydata(revisedarray):
   ##
   ##print(newcolorarray)
 
+  #quick, albeit incomprehensive check that everything worked well
   if len(newcolorarray)/3 == len(yarray):
     print('AMAZING. All seems kosher')
 
@@ -102,7 +111,7 @@ def formatmydata(revisedarray):
 
   #through the code above, we now successfully obtained the desired format.
   #the output of this code is stored in three arrays:
-  #newcolorarray: contains colors in base 7 form. r, g, and b values are stored separately in the array.
+  #newcolorarray: contains colors in base 255 form. r, g, and b values are stored separately in the array.
   #xarray: stores x coordinates as integers 0 through 31
   #yarray: stores y coordinates as integers 0 through 31
 
@@ -115,6 +124,8 @@ def lightemup(xarray, yarray, newcolorarray):
     r = r + 1
     t = t + 3
 
+#everything above was housed in defined functions, which must be called in order to be executed.
+#this last section controls the order in which our defined functions are executed
 indir = '/home/pi/PythonScripts/LEDMatrix/displays'
 while 0 < 1:
   for root, dirs, filenames in os.walk(indir):
@@ -126,7 +137,9 @@ while 0 < 1:
       lightemup(xaxis, yaxis, rgbcolor)
       time.sleep(20.0)
       matrix.Clear()
-    mailtestimaplib.themailfunction()
+    mailtestimaplib.themailfunction()#calls our email python file to look in email for new files
+    #NOTE: we are using syntax 'mailtestimaplib.themailfunction()' to indicate that we wish to 
+    #execute the 'themailfunction' in our file 'mailtestimaplib.py'
 
 
 
